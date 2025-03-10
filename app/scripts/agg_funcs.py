8042abde8fbe07bbd2e25d51063417d3432b2e12
Solarev Ruslan <ruslan251207@gmail.com>
2025-03-10 20:47:30 +1000

    Add comments

--- app/scripts/agg_funcs.py
+++ app/scripts/agg_funcs.py
@@ -2,12 +2,18 @@ import pandas as pd
 
 
 class DataAggregation:
+    """
+    Класс, создающий csv-файл с агрегационной информацией о пользователях и их действиях по дням
+    """
     def __init__(self, min_day: int, max_day: int, conn):
         self.min_day = min_day
         self.max_day = max_day
         self.conn = conn
     
     def get_new_accounts_count(self):
+        """
+        Получение кол-ва новых аккаунтов, созданных по дням, из базы данных
+        """
         new_accounts_count = pd.read_sql('''
         SELECT EXTRACT(DAY FROM created_at) AS p_day, COUNT(*)
         FROM "Users"
@@ -17,6 +23,11 @@ class DataAggregation:
         return new_accounts_count
     
     def get_message_anonymous_percentage(self, day: int):
+        """
+        Получение процентного отношения анонимных сообщений ко всем сообщениям за день из базы данных
+
+        :params day: День
+        """
         message_users = pd.read_sql(f'''
         SELECT u.*
         FROM "Content" AS co
@@ -35,6 +46,9 @@ class DataAggregation:
         return percentage
 
     def get_new_messages_count(self):
+        """
+        Получение общего кол-ва сообщений, написанных за день, из базы данных
+        """
         new_messages_count = pd.read_sql('''
         SELECT EXTRACT(DAY FROM created_at) AS p_day, COUNT(*)
         FROM "Content" co
@@ -45,6 +59,11 @@ class DataAggregation:
         return new_messages_count
     
     def get_new_topics_percentage(self, day: int):
+        """
+        Получение процентного отношения кол-ва новых тем за день ко всем существующим уже в базе данных
+
+        :params day: День
+        """
         prev_days_topics_count = pd.read_sql(f'''
         SELECT COUNT(*)
         FROM "Content" co
@@ -62,6 +81,9 @@ class DataAggregation:
         return percentage
 
     def get_agg_file(self):
+        """
+        Создание csv-файла, хранящего в себе агрегационную информацию о пользователях и их действиях по дням
+        """
         data = pd.DataFrame({"day": range(self.min_day, self.max_day + 1)})
 
         data["new_accounts"] = self.get_new_accounts_count()["count"].iloc[self.min_day-1:self.max_day-1]
--- app/scripts/generate_data.py
+++ app/scripts/generate_data.py
@@ -10,11 +10,19 @@ import json
 
 
 class DataGenerator:
+    """
+    Класс, генерирующий данные для созданной базы данных с логами
+    """
     def __init__(self, conn):
         self.conn = conn
-        self.faker = Faker()
+        self.faker = Faker().unique # объект класса Faker будет выдавать уникальные сгенерированные значения
 
     def insert_users(self, users: List[Tuple]):
+        """
+        Вставка пользователей в базу данных
+        
+        :params users: Список пользователей, содержащий username, password_hash, email и created_at для каждого пользователя
+        """
         try:
             sql = """INSERT INTO "Users" (username, password_hash, email, created_at)
             VALUES %s"""
@@ -28,6 +36,11 @@ class DataGenerator:
 
 
     def insert_actions(self, actions: List[Tuple]):
+        """
+        Вставка типов действий в базу данных
+
+        :params actions: Список типов действий, содержащий название типа для каждого действия
+        """
         try:
             sql = """INSERT INTO "Actions" (type)
             VALUES %s"""
@@ -42,6 +55,11 @@ class DataGenerator:
 
 
     def generate_users(self, created_at):
+        """
+        Генерация пользовательских данных (от 5 до 10 пользователей) и формирование списка с данными
+
+        :params created_at: Дата и время создания пользователей
+        """
         num_users = random.choice(range(5, 11))
 
         users = [(
@@ -60,6 +78,11 @@ class DataGenerator:
         self.insert_users(users)
 
     def get_action_id(self, action):
+        """
+        Получение ID типа действия по его названию из базы данных
+
+        :params action: Тип действия
+        """
         action_id = pd.read_sql(f'''
         SELECT action_id
         FROM "Actions"
@@ -67,8 +90,12 @@ class DataGenerator:
 
         return action_id
 
-
     def user_is_logined(self, user_id: int):
+        """
+        Проверка, залогинен ли пользователь (username != NULL) по его ID из базы данных
+
+        :params user_id: ID пользователя
+        """
         user_info = pd.read_sql(f'''
         SELECT * 
         FROM "Users"
@@ -78,6 +105,11 @@ class DataGenerator:
 
 
     def insert_topic(self, topic: Tuple):
+        """
+        Вставка темы в базу данных
+
+        :params topic: Кортеж Тема, содержащий название темы и время создания
+        """
         try:
             sql = """INSERT INTO "Content" (type, created_at)
             VALUES (%s, %s)
@@ -100,6 +132,11 @@ class DataGenerator:
 
 
     def insert_message(self, message: Tuple):
+        """
+        Вставка сообщения в базу данных
+
+        :params message: Кортеж Сообщение, содержащий текст сообщения, ID темы и время создания
+        """
         try:
             sql = """INSERT INTO "Content" (type, created_at)
             VALUES (%s, %s)
@@ -122,6 +159,11 @@ class DataGenerator:
 
 
     def insert_log(self, log: Tuple):
+        """
+        Вставка лога в базу данных
+
+        :params log: Кортеж Лог, содержащий ID пользователя, ID типа действия, ответ сервера (успех/провал) и время создания
+        """
         try:
             sql = """INSERT INTO "Logs" (user_id, action_id, is_success, created_at)
             VALUES (%s, %s, %s, %s)
@@ -140,6 +182,12 @@ class DataGenerator:
 
 
     def insert_content_log(self, log_id: int, content_id: int):
+        """
+        Вставка связи лог-контент в базу данных
+
+        :params log_id: ID лога
+        :params content_id: ID контента (сообщение/тема)
+        """
         try:
             sql = """INSERT INTO "Content_Log" (log_id, content_id)
             VALUES (%s, %s);"""
@@ -152,27 +200,36 @@ class DataGenerator:
             self.conn.close()
 
     def generate_data(self):
+        """
+        Генерация данных, используя случайные значения, за январь 2025 года
+        """
+        # Получение названий типов действий
         with open('data/actions.json', 'r') as file:
             actions_json = json.load(file)
             actions = [(action["name"],) for action in actions_json["actions"]]
 
+        # Вставка типов действий и чтение из базы данных
         self.insert_actions(actions)
         actions = pd.read_sql('SELECT * FROM "Actions"', self.conn)["type"].tolist()
 
+        # Цикл для генерации данных на каждый день
         for day in range(1, 32):
-            created_at = datetime(year=2025, month=1, day=day)
+            topic_error_counter = 0 # Кол-во ошибок при создании темы
+            created_at = datetime(year=2025, month=1, day=day) # Дата и время создания
 
-            self.generate_users(created_at)
+            self.generate_users(created_at) # Генерация пользователей
 
             users_id = pd.read_sql('SELECT user_id FROM "Users"', self.conn)["user_id"].tolist()
 
+            # Генераций по 5-10 действий каждого типа
             for action in actions:
                 action_id = self.get_action_id(action)
                 num_actions = random.choice(range(5, 11))
 
                 for i in range(num_actions):
-                    user_id = random.choice(users_id)
+                    user_id = random.choice(users_id) # Случайный выбор ID пользователя из базы данных
                     if action == "Создание темы":
+                        # Проверка: залогинен ли пользователь?
                         if self.user_is_logined(user_id):
                             is_success = True
                             topic_title = self.faker.sentence()
@@ -181,12 +238,15 @@ class DataGenerator:
                             self.insert_content_log(log_id, content_id)
                         else:
                             is_success = False
+                            topic_error_counter +=1
                             self.insert_log((user_id, action_id, is_success, created_at))
                     
                     elif action == "Написание сообщения":
                         is_success = True
+                        # Получение случайного ID темы из базы данных
                         topics_id = pd.read_sql('SELECT content_id FROM "Content" WHERE type = \'Тема\'', self.conn)["content_id"].tolist()
                         topic_id = random.choice(topics_id)
+
                         message_content = self.faker.sentence()
                         content_id = self.insert_message((message_content, topic_id, created_at))
                         log_id = self.insert_log((user_id, action_id, is_success, created_at))
@@ -195,3 +255,15 @@ class DataGenerator:
                     else:
                         is_success = True
                         self.insert_log((user_id, action_id, is_success, created_at))
+                
+                # Проверка: произошли ли как минимум 2 ошибки при создании темы
+                if action == "Создание темы" and topic_error_counter < 2:
+                    # Получение всех ID незалогиненных пользователей
+                    anonymus_users = pd.read_sql('SELECT user_id FROM "Users" WHERE username is NULL', self.conn)["user_id"].tolist()
+                    while (topic_error_counter < 2):
+                        user_id = random.choice(anonymus_users)
+                        is_success = False
+                        self.insert_log((user_id, action_id, is_success, created_at))
+                        topic_error_counter += 1
+                    
+                
